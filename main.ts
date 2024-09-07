6aa32fcf1df01d56e5b76fe3d48b6603b956bcc5
Hasnain-Software-Developer <163559600+Hasnain-Software-Developer@users.noreply.github.com>
2024-09-08 02:52:34 +0500

    Files

---
+++ main.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node          
+console.log("\n\t Welcome to Hasnain's Coding World\n");
+
+import inquirer from "inquirer"
+ 
+let input1 = await inquirer.prompt([
+    {
+        message:"Enter Your First Number",
+        type: "number",
+        name:"firstNumber"
+    },
+    {
+        message:"Enter Your Second Number",
+        type: "number",
+        name:"SecondNumber"
+    },
+    {
+        message:"Select the Operator to perform Actions",
+        type: "list",
+        name:"Operator",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+    }
+])
+if (input1.Operator ==="Addition") {
+    console.log(input1.firstNumber + input1.SecondNumber);    
+}
+else if (input1.Operator === "Subtraction"){
+    console.log(input1.firstNumber-input1.SecondNumber);   
+}
+else if (input1.Operator === "Multiplication"){
+    console.log(input1.firstNumber * input1.SecondNumber);   
+}
+else if (input1.Operator === "Division"){
+    console.log(input1.firstNumber/input1.SecondNumber);   
+}
+else {console.log("Choose The correct Operator And try Again");
+}
